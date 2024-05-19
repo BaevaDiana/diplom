0c0f81523a3e87ad5461dc21189473d33dffde26
pluxurypinkphloydrockstar <thedianab15@gmail.com>
2024-05-19 18:08:38 +0300

    fixs

--- Fuzzy_C-Means.py
+++ Fuzzy_C-Means.py
@@ -13,21 +13,22 @@ from tkinter import filedialog, messagebox
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-# Загрузка списка стоп-слов и инициализация лемматизатора
+# загрузка списка стоп-слов
 stop_words_en = set(stopwords.words('english'))
 stop_words_ru = set(stopwords.words('russian'))
+# инициализация лемматизатора
 lemmatizer = WordNetLemmatizer()
 
 
 def preprocess_text(text):
-    # Токенизация текста
+    # токенизация текста
     tokens = word_tokenize(text.lower())
 
-    # Удаление пунктуации и стоп-слов, а также лемматизация
+    # фильтрация токенов, а также лемматизация
     tokens = [lemmatizer.lemmatize(word) for word in tokens if word.isalnum()
               and word not in stop_words_en and word not in stop_words_ru]
 
-    # Объединение токенов обратно в текст
+    # объединение токенов обратно в текст
     preprocessed_text = ' '.join(tokens)
 
     return preprocessed_text
@@ -35,24 +36,25 @@ def preprocess_text(text):
 
 def extract_text_from_docx(docx_path):
     try:
-        # Извлечение текста из документа формата .docx
+        # извлечение текста из документа формата .docx
         text = docx2txt.process(docx_path)
         return text
     except Exception as e:
         messagebox.showerror("Ошибка!", f"Произошла ошибка при извлечении текста из Word: {e}")
         return None
 
+
 def extract_text_from_pdf(pdf_path):
     try:
-        # Открытие PDF-файла
+        # открытие PDF-файла
         with open(pdf_path, 'rb') as file:
-            # Создание объекта PDFReader
+            # создание объекта PDFReader
             pdf_reader = PyPDF2.PdfReader(file)
-            # Инициализация переменной для хранения текста
+            # инициализация переменной для хранения текста
             text = ""
-            # Чтение каждой страницы PDF-файла
+            # чтение каждой страницы PDF-файла
             for page in pdf_reader.pages:
-                # Извлечение текста с каждой страницы и добавление его к общему тексту
+                # извлечение текста с каждой страницы и добавление его к общему тексту
                 text += page.extract_text()
             return text
     except Exception as e:
@@ -60,28 +62,30 @@ def extract_text_from_pdf(pdf_path):
         return None
 
 
-
 def load_documents(directory):
     documents = []
     document_names = []
     for filename in os.listdir(directory):
+        # обработка документа формата .txt
         if filename.endswith(".txt"):
             with open(os.path.join(directory, filename), 'r', encoding='utf-8') as file:
                 documents.append(preprocess_text(file.read()))
                 document_names.append(filename)
+        # обработка документа формата .docx
         elif filename.endswith(".docx"):
             text = extract_text_from_docx(os.path.join(directory, filename))
             if text:
                 documents.append(preprocess_text(text))
                 document_names.append(filename)
+        # обработка документа формата .pdf
         elif filename.endswith(".pdf"):
             text = extract_text_from_pdf(os.path.join(directory, filename))
             if text:
                 documents.append(preprocess_text(text))
                 document_names.append(filename)
+        # нетекстовые документы игнорируются
         else:
-            messagebox.showerror("Ошибка!", "В папке должны быть текстовые документы.")
-            return None, None
+            continue
     return documents, document_names
 
 
@@ -89,37 +93,30 @@ def cluster_documents(documents):
     if not documents:
         messagebox.showerror("Ошибка!", "Нет документов для кластеризации.")
         return None, None
-    # Создание объекта TfidfVectorizer для преобразования текстовых документов в матрицу TF-IDF
-    # max_df и min_df устанавливают пороговые значения для отбора слов в TF-IDF матрице
-    vectorizer = TfidfVectorizer(max_df=0.9, min_df=0.1)
 
-    # Преобразование текстовых документов в TF-IDF матрицу
+    # создание объекта TfidfVectorizer для преобразования текстовых документов в матрицу TF-IDF
+    vectorizer = TfidfVectorizer(max_df=0.9, min_df=0.1)
+    # преобразование текстовых документов в TF-IDF матрицу
     tfidf_matrix = vectorizer.fit_transform(documents)
 
-    # Создание объекта TruncatedSVD для уменьшения размерности TF-IDF матрицы до 100 компонент
+    # создание объекта TruncatedSVD для уменьшения размерности TF-IDF матрицы
     svd = TruncatedSVD(n_components=100)
-
-    # Применение метода fit_transform к TF-IDF матрице для уменьшения размерности
+    # применение метода fit_transform к TF-IDF матрице для уменьшения размерности
     tfidf_reduced = svd.fit_transform(tfidf_matrix)
 
-    # Создание объекта FCM (Fuzzy C-Means) для кластеризации уменьшенной TF-IDF матрицы
+    # создание объекта FCM (Fuzzy C-Means) для кластеризации
     fcm = FCM()
-
-    # Применение метода fit к уменьшенной TF-IDF матрице для обучения модели FCM
+    # обучение модели FCM
     fcm.fit(tfidf_reduced)
-
-    # Получение меток кластеров
+    # получение меток кластеров
     labels = fcm.predict(tfidf_reduced)
 
-    # Вычисляем силуэт для каждого документа
-    # silhouette_values = silhouette_samples(tfidf_matrix, labels)
-
-    # Если у нас есть только один кластер, обработаем его отдельно
+    # вычисление силуэта для каждого документа
     if len(set(labels)) < 2:
-        messagebox.showwarning("Предупреждение!", "Всего один кластер.")
+        # отдельная обработка случая с одним кластером
+        messagebox.showwarning("Предупреждение!", "Выделен всего один кластер.")
         silhouette_values = 0
     else:
-        # Вычисляем силуэт для каждого документа
         silhouette_values = silhouette_samples(tfidf_matrix, labels)
 
     return labels, silhouette_values
@@ -132,13 +129,16 @@ def browse_button():
 
 
 def visualize_clusters():
+    # получение директории
     directory = documents_directory.get()
     if not directory:
         messagebox.showerror("Ошибка!", "Выберите папку с текстовыми документами.")
         return
 
+    # загрузка файлов
     documents, document_names = load_documents(directory)
     if documents is not None:
+        # кластерный анализ
         labels, silhouette_values = cluster_documents(documents)
         if labels is not None and silhouette_values is not None:
             if len(set(labels)) < 2:
@@ -174,6 +174,7 @@ def visualize_clusters():
             canvas = FigureCanvasTkAgg(fig, master=graph_frame)
             canvas.draw()
             canvas.get_tk_widget().pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+
 
 # кнопка "Очистить"
 def clear_data():
@@ -216,11 +217,12 @@ clear_button.pack(side=tk.RIGHT, padx=225, pady=15)
 text_frame = tk.LabelFrame(root, text="Кластеры",font=("Open Sans", 10))
 text_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True, padx=5, pady=5)
 
-graph_frame = tk.LabelFrame(root, text="График", font=("Open Sans", 10),)
+graph_frame = tk.LabelFrame(root, text="График", font=("Open Sans", 10))
 graph_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True, padx=5, pady=5)
 
 # текстовое поле для вывода информации о кластерах
 text_output = tk.Text(text_frame, height=10, width=20)
 text_output.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
+# запуск окна
 root.mainloop()
