907310a8698272b3035296b0601189833a6a4ce8
pluxurypinkphloydrockstar <thedianab15@gmail.com>
2024-06-02 21:34:34 +0300

    renames files

--- chernovik_fuzzy_C-Means_2.py
+++ chernovik_fuzzy_C-Means_2.py
@@ -6,14 +6,12 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
+from fcmeans import FCM
 from sklearn.metrics import silhouette_samples
-import numpy as np
-import skfuzzy as fuzz
 import tkinter as tk
 from tkinter import filedialog, messagebox
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import shutil
 
 # загрузка списка стоп-слов
 stop_words_en = set(stopwords.words('english'))
@@ -21,11 +19,12 @@ stop_words_ru = set(stopwords.words('russian'))
 # инициализация лемматизатора
 lemmatizer = WordNetLemmatizer()
 
+
 def preprocess_text(text):
     # токенизация текста
     tokens = word_tokenize(text.lower())
 
-    # фильтрация токенов и лемматизация
+    # фильтрация токенов, а также лемматизация
     tokens = [lemmatizer.lemmatize(word) for word in tokens if word.isalnum()
               and word not in stop_words_en and word not in stop_words_ru]
 
@@ -91,13 +90,11 @@ def load_documents(directory):
     return documents, document_names
 
 
-# кластеризация с использованием нечеткой нейронной сети
 def cluster_documents(documents):
     if not documents:
         messagebox.showerror("Ошибка!", "Нет документов для кластеризации.")
         return None, None
 
-    # векторизация текста
     # создание объекта TfidfVectorizer для преобразования текстовых документов в матрицу TF-IDF
     vectorizer = TfidfVectorizer(max_df=0.9, min_df=0.1)
     # преобразование текстовых документов в TF-IDF матрицу
@@ -108,48 +105,22 @@ def cluster_documents(documents):
     # применение метода fit_transform к TF-IDF матрице для уменьшения размерности
     tfidf_reduced = svd.fit_transform(tfidf_matrix)
 
-    # создание массива данных
-    data = np.array(tfidf_reduced)
-    # критерий останова
-    min_delta_fpc = 0.001
-    prev_fpc = -np.inf
-
-    # поиск оптимального количества кластеров
-    # начальное значение параметра нечеткости
-    m = 1.1
-    k = 1
-    while True:
-        # обучение нечеткой нейронной сети
-        cntr, u, u0, d, jm, p, fpc = fuzz.cluster.cmeans(
-            data.T, c=k, m=m, error=0.005, maxiter=1000, init=None
-        )
-
-        # проверка критерия останова
-        if abs(fpc - prev_fpc) < min_delta_fpc:
-            break
-
-        # обновление параметра нечеткости
-        prev_fpc = fpc
-        m += 0.1
-        k+=1
-
-    # преобразование результатов в метки кластеров
-    labels = np.argmax(u, axis=0)
-
-    # преобразование меток в непрерывные индексы начиная с 0
-    unique_labels = np.unique(labels)
-    label_map = {old_label: new_label for new_label, old_label in enumerate(unique_labels)}
-    continuous_labels = np.array([label_map[label] for label in labels])
+    # создание объекта FCM (Fuzzy C-Means) для кластеризации
+    fcm = FCM()
+    # обучение модели FCM
+    fcm.fit(tfidf_reduced)
+    # получение меток кластеров
+    labels = fcm.predict(tfidf_reduced)
 
     # вычисление силуэта для каждого документа
-    if len(set(continuous_labels)) < 2:
+    if len(set(labels)) < 2:
         # отдельная обработка случая с одним кластером
         messagebox.showwarning("Предупреждение!", "Выделен всего один кластер.")
         silhouette_values = 0
     else:
-        silhouette_values = silhouette_samples(tfidf_matrix, continuous_labels)
+        silhouette_values = silhouette_samples(tfidf_matrix, labels)
 
-    return continuous_labels, silhouette_values
+    return labels, silhouette_values
 
 
 def browse_button():
@@ -183,12 +154,13 @@ def visualize_clusters():
 
             text_output.delete(1.0, tk.END)
 
+
             # информация о кластерах и силуэтах в текстовое поле
             text_output.insert(tk.END, "Документы, разбитые на кластеры:\n")
             silhouette_dict = {i: [] for i in set(labels)}
             for doc_name, label, silhouette in zip(document_names, labels, silhouette_values):
                 silhouette_dict[label].append((doc_name, silhouette))
-            # вывод кластеров по порядку, название документа и его значение силуэта
+            # вывод кластеры по порядку, название документа и его значение силуэта
             for cluster in sorted(silhouette_dict.keys()):
                 text_output.insert(tk.END, f"\nКластер {cluster}:\n")
                 for doc_name, silhouette in silhouette_dict[cluster]:
@@ -213,66 +185,6 @@ def visualize_clusters():
             canvas.get_tk_widget().pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
 
 
-def create_cluster_folders():
-    # получение директории
-    directory = documents_directory.get()
-    if not directory:
-        messagebox.showerror("Ошибка!", "Выберите папку с документами.")
-        return
-
-    # определение родительской директории
-    parent_directory = os.path.dirname(directory)
-    if not parent_directory:
-        messagebox.showerror("Ошибка!", "Не удалось определить родительскую директорию.")
-        return
-
-    # загрузка файлов
-    documents, document_names = load_documents(directory)
-    if documents is not None:
-        # кластерный анализ
-        labels, _ = cluster_documents(documents)
-        if labels is not None:
-            cluster_dirs = {}
-            for label in set(labels):
-                # создание папок для кластеров
-                cluster_dir = os.path.join(parent_directory, f"Cluster_{label}")
-                os.makedirs(cluster_dir, exist_ok=True)
-                cluster_dirs[label] = cluster_dir
-
-            for doc_name, label in zip(document_names, labels):
-                # копирование файлов в соответствующие папки кластеров
-                src_path = os.path.join(directory, doc_name)
-                dest_path = os.path.join(cluster_dirs[label], doc_name)
-                shutil.copy2(src_path, dest_path)
-
-            messagebox.showinfo("Выполнено", "Папки кластеров успешно созданы и файлы скопированы.")
-
-
-def delete_cluster_folders():
-    # получение директории
-    directory = documents_directory.get()
-    if not directory:
-        messagebox.showerror("Ошибка!", "Выберите папку с документами.")
-        return
-
-    # определение родительской директории
-    parent_directory = os.path.abspath(os.path.join(directory, os.pardir))
-    if not parent_directory:
-        messagebox.showerror("Ошибка!", "Не удалось определить родительскую директорию.")
-        return
-
-    # удаление директорий кластеров
-    documents, document_names = load_documents(directory)
-    if documents is not None:
-        labels, _ = cluster_documents(documents)
-        if labels is not None:
-            for label in set(labels):
-                cluster_dir = os.path.join(parent_directory, f"Cluster_{label}")
-                if os.path.exists(cluster_dir):
-                    shutil.rmtree(cluster_dir)
-
-            messagebox.showinfo("Выполнено", "Папки кластеров успешно удалены.")
-
 # кнопка "Очистить"
 def clear_data():
     text_output.delete(1.0, tk.END)
@@ -281,6 +193,7 @@ def clear_data():
     graph_frame.destroy()
     graph_frame = tk.LabelFrame(root, text="График", font=("Open Sans", 10))
     graph_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True, padx=5, pady=5)
+
 
 # настройка параметров окна
 root = tk.Tk()
@@ -301,19 +214,13 @@ browse_button.pack(padx=7, pady=5)
 
 # фрейм для кнопок
 button_frame = tk.Frame(root, bg="azure4")
-button_frame.pack(fill=tk.BOTH)
+button_frame.pack(fill=tk.X)
 
 cluster_button = tk.Button(button_frame, text="Кластеризация", command=visualize_clusters, bg="grey75", fg="black", font=("Open Sans", 10, "bold"), cursor="hand2")
-cluster_button.pack(side=tk.LEFT, padx=(200, 100), pady=15)
-
-create_folders_button = tk.Button(button_frame, text="Создать папки", command=create_cluster_folders, bg="grey75", font=("Open Sans", 10, "bold"), cursor="hand2")
-create_folders_button.pack(side=tk.LEFT, padx=(100, 0), pady=15)
+cluster_button.pack(side=tk.LEFT, padx=225, pady=15)
 
 clear_button = tk.Button(button_frame, text="Очистить", command=clear_data, bg="grey75", fg="black", font=("Open Sans", 10, "bold"), cursor="hand2")
-clear_button.pack(side=tk.RIGHT, padx=(0, 100), pady=15)
-
-delete_folders_button = tk.Button(button_frame, text="Удалить папки", command=delete_cluster_folders, bg="grey75", font=("Open Sans", 10, "bold"), cursor="hand2")
-delete_folders_button.pack(side=tk.RIGHT, padx=(100, 200), pady=15)
+clear_button.pack(side=tk.RIGHT, padx=225, pady=15)
 
 # рамка для текстового поля и графика
 text_frame = tk.LabelFrame(root, text="Кластеры",font=("Open Sans", 10))
